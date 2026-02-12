bb6c2bf1a26b82c33f595ae9c89f742eab672d29
Vadaparty <svadaparty@trcsolutions.com>
2026-02-12 16:10:40 +0530

    fixed env issues

--- app/config.py
+++ app/config.py
@@ -2,7 +2,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-env_path = Path(__file__).resolve().parent / '.env'
+# Load .env from project root
+env_path = Path(__file__).resolve().parent.parent / '.env'
 load_dotenv(env_path)
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
